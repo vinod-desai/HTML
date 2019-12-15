f311ffa20c5522090d7280c1fa58a67908174f7c
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2019-12-15 16:59:18 +0530

    CSS Tricks

---
+++ HTML CSS Tricks/app.js
@@ -0,0 +1,5 @@
+const clickHereButton = document.getElementById('clickHereBtn');
+let handleClickHereButton = function(){
+    alert('Button Clicked!!');
+}
+clickHereButton.addEventListener('click', handleClickHereButton);
